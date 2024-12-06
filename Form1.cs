383d42a9e88cd3f81328b0b56f8274fbe7cb0dfb
juan Ca <cerezorojasjuancamilo@gmail.com>
2024-12-06 16:18:55 -0500

    añadir archivos

---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private double valor1;
+        private double valor2;
+
+        private double resultado;
+
+        private int operacion;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Boton suma
+            operacion = 1;
+            valor1 = Convert.ToDouble(tbDisplay.Text);
+            tbDisplay.Text = "";
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            //numero 0 
+            tbDisplay.Text = tbDisplay.Text + "0";
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            //numero 1
+            tbDisplay.Text = tbDisplay.Text + "1";
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            //numero 2
+            tbDisplay.Text = tbDisplay.Text + "2";
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            //numero 3
+            tbDisplay.Text = tbDisplay.Text + "3";
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            //numero 4
+            tbDisplay.Text = tbDisplay.Text + "4";
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            //numero 5
+            tbDisplay.Text = tbDisplay.Text + "5";
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            //numero 6
+            tbDisplay.Text = tbDisplay.Text + "6";
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            //numero 7
+            tbDisplay.Text = tbDisplay.Text + "7";
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            //numero 8
+            tbDisplay.Text = tbDisplay.Text + "8";
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            //numero 9
+            tbDisplay.Text = tbDisplay.Text + "9";
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            //boton Declaine
+            tbDisplay.Text = "";
+        }
+
+        private void btnIgual_Click(object sender, EventArgs e)
+        {
+            //boton Igual
+            string digit = tbDisplay.Text;
+            if (digit == "")
+            {
+                MessageBox.Show("La caja esta vacia");
+            }
+            else
+            {
+                valor2 = Convert.ToDouble(digit);
+            }
+
+            switch (operacion)
+            {
+                case 1:
+                    resultado = valor1 + valor2;
+                    break;
+                case 2:
+                    resultado = valor1 - valor2;
+                    break;
+                case 3:
+                    resultado = valor1 * valor2;
+                    break;
+                case 4:
+                    resultado = valor1 / valor2;
+                    break;
+                case 5:
+                    resultado = CalcularPotencia(valor1, valor2);
+                    break;
+
+            }
+
+            tbDisplay.Text = resultado.ToString();
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            //boton Resta
+            operacion = 2;
+            valor1 = Convert.ToDouble(tbDisplay.Text);
+            tbDisplay.Text = "";
+        }
+
+        private void btnPunto_Click(object sender, EventArgs e)
+        {
+            //boton Punto
+            tbDisplay.Text = tbDisplay.Text + ".";
+        }
+
+        private void btnMultiplicacion_Click(object sender, EventArgs e)
+        {
+            //boton Multiplicacion
+            operacion = 3;
+            valor1 = Convert.ToDouble(tbDisplay.Text);
+            tbDisplay.Text = "";
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            //boton DIVISION
+            operacion = 4;
+            valor1 = Convert.ToDouble(tbDisplay.Text);
+            tbDisplay.Text = "";
+        }
+
+        private void btnPotencia_Click(object sender, EventArgs e)
+        {
+            //Boton Potencia
+            operacion = 5;
+            valor1 = Convert.ToDouble(tbDisplay.Text);
+            tbDisplay.Text = "";
+
+        }
+        private double CalcularPotencia(double BaseNum, double exponente)
+        {
+            double resultado = 1;
+            for (int i = 0; i < exponente; i++)
+            {
+                resultado *= BaseNum; // Multiplicamos la base por sí misma
+            }
+            return resultado;
+        }
+                      
+        
+    }
+}
